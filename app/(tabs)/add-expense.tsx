17ac14cefa29e523935ff4d0c25505cd7d623147
Okinawa <fitthep@gmail.com>
2025-04-17 22:42:05 +0700

    create add expense and income

---
+++ app/(tabs)/add-expense.tsx
@@ -0,0 +1,10 @@
+// app/(tabs)/add-expense.tsx
+import { View, Text } from "react-native";
+
+export default function AddExpenseScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>➖ Add Expense Page</Text>
+    </View>
+  );
+}
