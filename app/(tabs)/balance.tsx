e5584e20915685d51af1fcc4bc3c891bea6e4ab3
Okinawa <fitthep@gmail.com>
2025-04-17 22:40:41 +0700

    create balance

---
+++ app/(tabs)/balance.tsx
@@ -0,0 +1,10 @@
+// app/(tabs)/balance.tsx
+import { View, Text } from "react-native";
+
+export default function BalanceScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>💰 Balance Page</Text>
+    </View>
+  );
+}
--- app/(tabs)/statistic.tsx
+++ app/(tabs)/statistic.tsx
@@ -1,9 +1,10 @@
 // app/(tabs)/statistic.tsx
-import { Text, View } from "react-native";
+import { View, Text } from 'react-native';
+
 export default function StatisticScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Statistic Page</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>📊 Statistic Page</Text>
     </View>
   );
 }
