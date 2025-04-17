7cbfcc809e0c345bbe194c0ada982d09ab322dbc
Okinawa <fitthep@gmail.com>
2025-04-17 22:35:54 +0700

    create statistic

---
+++ app/(tabs)/statistic.tsx
@@ -0,0 +1,9 @@
+// app/(tabs)/statistic.tsx
+import { Text, View } from "react-native";
+export default function StatisticScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Statistic Page</Text>
+    </View>
+  );
+}
